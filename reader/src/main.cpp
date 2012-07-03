002f5f82e86532c5dc67572d9619f9b9d73e3391
Wouter Cloete <wouter.cloete@firmlogik.co.za>
2012-07-03 07:31:59 +0200

    Cosmetic

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -11,7 +11,6 @@ extern "C" {
 #include "socket.h"
 #include "dhcp.h"
 }
-
 /****************************************************************************************/
 #include "main.h"
 #include "crc.h"
